a5baabe246b8c352a50e87aebdca124989175a9f
griceldavaldez <gvaldez@sodep.com.py>
2023-05-25 11:22:06 -0400

    Agrega métodos para convertir Entity a DTO y viceversa

---
+++ src/main/java/io/github/jokoframework/securitystarterbackend/mapper/UserMapper.java
@@ -0,0 +1,38 @@
+package io.github.jokoframework.securitystarterbackend.mapper;
+
+import io.github.jokoframework.securitystarterbackend.dto.request.UserRequestDTO;
+import io.github.jokoframework.securitystarterbackend.dto.response.UserResponseDTO;
+import io.github.jokoframework.securitystarterbackend.entities.UserEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserMapper {
+    public static UserEntity toEntity(UserRequestDTO requestDTO) {
+        UserEntity entity = new UserEntity();
+        entity.setUsername(requestDTO.getUsername());
+        entity.setEmail(requestDTO.getEmail());
+        entity.setPassword(requestDTO.getPassword());
+        return entity;
+    }
+
+    public static UserResponseDTO toResponseDTO(UserEntity entity) {
+        UserResponseDTO responseDTO = new UserResponseDTO();
+        responseDTO.setUsername(entity.getUsername());
+        responseDTO.setEmail(entity.getEmail());
+        responseDTO.setStatus(entity.getStatus());
+        responseDTO.setCreationDate(entity.getCreationDate());
+        responseDTO.setLastAccess(entity.getLastAccess());
+        return responseDTO;
+    }
+
+    public static List<UserResponseDTO> toResponseDTOList(List<UserEntity> entities) {
+        List<UserResponseDTO> dtos = new ArrayList<>();
+
+        for (UserEntity entity : entities) {
+            UserResponseDTO dto = toResponseDTO(entity);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+}
