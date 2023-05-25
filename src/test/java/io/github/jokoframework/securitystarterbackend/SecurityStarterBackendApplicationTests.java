3fdca17c7ed67be70d3280b0612349a643dcfea1
griceldavaldez <gvaldez@sodep.com.py>
2023-05-25 11:35:33 -0400

    Agrega recursos y archivos de configuraciones necesarios para ejecutar el proyecto

---
+++ src/test/java/io/github/jokoframework/securitystarterbackend/SecurityStarterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jokoframework.securitystarterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityStarterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
